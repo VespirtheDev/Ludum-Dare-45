9a085a106686849bc73976c34714c223e47a19d2
MrSearchEngine <mrsearchengine@gmail.com>
2019-10-06 15:30:04 -0400

    Finished making the level tileset work.

--- _Final/Scenes/Test.gd
+++ _Final/Scenes/Test.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 func _ready():
 	$Player.can_run_right = true
-	$LevelUpgradeAnim.play("ShowWhiteBackground")
+	#$LevelUpgradeAnim.play("ShowWhiteBackground")
 
 func player_death():
 	$Player.global_position = $SpawnPoint.global_position
@@ -11,31 +11,31 @@ func player_death():
 func upgrade_game(id):
 	if id == 1:
 		$Player.can_run_left = true
-		$LevelUpgradeAnim.play("BlackLineGround")
+		#$LevelUpgradeAnim.play("BlackLineGround")
 	elif id == 2:
 		$Player.can_jump = true
-		$LevelUpgradeAnim.play("Autotile")
+		#$LevelUpgradeAnim.play("Autotile")
 		#Spawn enemies
 	elif id == 3:
 		$Player.can_crouch = true
-		$LevelUpgradeAnim.play("BlanktoGrassTiles")
+		#$LevelUpgradeAnim.play("BlanktoGrassTiles")
 	elif id == 4:
 		$Player.can_sprint = true
-		$LevelUpgradeAnim.play("ActivateParticles")
+		#$LevelUpgradeAnim.play("ActivateParticles")
 	elif id == 5:
 		$Player.can_climb = true
 		$Player.can_interact = true
-		$LevelUpgradeAnim.play("SpaceBackground")
+		#$LevelUpgradeAnim.play("SpaceBackground")
 	elif id == 6:
 		$Player.can_crawl = true
-		$LevelUpgradeAnim.play("ShowDecor")
+		#$LevelUpgradeAnim.play("ShowDecor")
 	elif id == 7:
 		$Player.can_wall_jump = true
-		$LevelUpgradeAnim.play("ShowParallax")
+		#$LevelUpgradeAnim.play("ShowParallax")
 	elif id == 8:
 		$Player.jump_count_max = 2
 	elif id == 9:
 		$Player.slow_fall = true
-		$LevelUpgradeAnim.play("HugeStarReveal")
+		#$LevelUpgradeAnim.play("HugeStarReveal")
 
 
