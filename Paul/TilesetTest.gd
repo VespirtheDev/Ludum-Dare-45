cd18fab2c0b4936c22b78d7b16a8f6e875339c59
Clarencerainsford <45524772+Clarencerainsford@users.noreply.github.com>
2019-10-05 23:55:09 -0400

    Potential Particle effect

--- Paul/TilesetTest.gd
+++ Paul/TilesetTest.gd
@@ -4,4 +4,7 @@ func _process(delta):
 	if Input.is_action_pressed("test1"):
 		$AnimationPlayer.play("grass")
 	if Input.is_action_pressed("test2"):
+		$CanvasLayer/Particles2D.emitting = true
+	if Input.is_action_pressed("test3"):
+		$CanvasLayer/Particles2D.emitting = false
 		$AnimationPlayer.play("Background")
